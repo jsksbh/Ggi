f09c041f70ed315c3c7b833c9ca626e42971d4fb
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-06 23:39:47 +0200

    Update hamya.py

--- AFROTOMusic/plugins/play/hamya.py
+++ AFROTOMusic/plugins/play/hamya.py
@@ -88,7 +88,7 @@ async def tard(_: Client, message: Message):
             return
      else: await message.reply("- يجب ان تكون ادمن على الاقل لإستخدام هذا الامر.", reply_to_message_id=message.message_id)
 
-@app.on_message(filters.command(["unban"]))
+@app.on_message(filters.command(["الغاء حظر"]))
 def unban_users(client, message):
     # Check if the user is an admin
     if message.from_user.id == message.chat.id:
