e2b94dd5b79633cd13709f5c356ff933378224c9
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-06 22:02:33 +0200

    Update hamya.py

--- AFROTOMusic/plugins/play/hamya.py
+++ AFROTOMusic/plugins/play/hamya.py
@@ -31,12 +31,14 @@ async def unktm(_: Client, message: Message):
         if member["result"]["status"] == "administrator":
             if message.reply_to_message.from_user.id not in muted: return await message.reply("- هذا المستخدم غير مكتوم!")
             muted.remove(message.reply_to_message.from_user.id)
-            await message.reply("- تم الغاء كتم العضو بنجاح!", reply_to_message_id=message.message_id)
+            await message.reply_text(f"تم الغاء  العضو\n│ \n : {message.reply_to_message.from_user.mention}\n\n بنجاح ")
+
             return
         elif member["result"]["status"] == "creator":
             if message.reply_to_message.from_user.id not in muted: return await message.reply("- هذا المستخدم غير مكتوم!")
             muted.remove(message.reply_to_message.from_user.id)
-            await message.reply("- تم الغاء كتم العضو بنجاح!", reply_to_message_id=message.message_id)
+            await message.reply_text(f"تم الغاء  العضو\n│ \n : {message.reply_to_message.from_user.mention}\n\n بنجاح ")
+
             return
         else: await message.reply("- يجب ان تكون ادمن على الاقل لإستخدام هذا الامر.", reply_to_message_id=message.message_id)
 
@@ -52,10 +54,12 @@ async def maktom(_: Client, message: Message):
 @app.on_message(filters.command("مسح المكتومين", ""))
 async def ms7maktom(_: Client, message: Message):
     member = member = requests.get(f"https://api.telegram.org/bot{app.bot_token}/getChatMember?chat_id={message.chat.id}&user_id={message.from_user.id}").json()
-    if member["result"]["status"] not in ["creator", "administrator"]: return await message.reply("- يجب ان تكون ادمن على الاقل لإستخدام الامر.", reply_to_message_id=message.message_id)
+    if member["result"]["status"] not in ["creator", "administrator"]: return await message.reply_text(f"يجب ان تكون ادمن علي الاقل لاستخدام هذا الامر\n│ \n : {message.reply_to_message.from_user.mention}\n\n بنجاح ")
+        
     if not len(muted): return await message.reply("- لا يوجد مكتومين لحذفهم!")
     muted.clear()
-    await message.reply("- تم مسح المكتومين.", reply_to_message_id=message.message_id)
+    await message.reply_text(f"تم مسح المكتومين \n│ \n : {message.reply_to_message.from_user.mention}\n\n بنجاح ")
+
     
 
 @app.on_message(filters.text & filters.group, group=928)
@@ -71,12 +75,15 @@ async def tard(_: Client, message: Message):
         if member["result"]["status"] == "administrator":
             if memberB["result"]["status"] in ["creator", "administrator"]:return await message.reply("- لا يمكنك طرد مشرف او مالك", reply_to_message_id=message.message_id)
             try:await app.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-            except: return await message.reply("- ليس لدي الصلاحيه لطرد هذا العضو")
-            await message.reply("- تم طرد العضو بنجاح!", reply_to_message_id=message.message_id)
+            except: return await message.reply_text(f"ليس لدي صلاحيات لطرد هذا العضو\n│ \n : {message.reply_to_message.from_user.mention}\n\n بنجاح ")
+
+            await message.reply_text(f"تم طرد العضو \n│ \n : {message.reply_to_message.from_user.mention}\n\n بنجاح ")
+
             return
         elif member["result"]["status"] == "creator":
             try:await app.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-            except: return await message.reply("- ليس لدي الصلاحيه لطرد هذا العضو")
+            except: return await message.reply_text(f"ليس لدي صلاحيات لطرد العضو\n│ \n : {message.reply_to_message.from_user.mention}\n\n  بنجاح")
+
             await message.reply("- تم طرد العضو بنجاح!", reply_to_message_id=message.message_id)
             return
         else: await message.reply("- يجب ان تكون ادمن على الاقل لإستخدام هذا الامر.", reply_to_message_id=message.message_id)
