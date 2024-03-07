d3a5dcc1ad5512626725575066e752f0d783012a
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-07 02:11:04 +0200

    Update userbot.py

--- AFROTOMusic/core/userbot.py
+++ AFROTOMusic/core/userbot.py
@@ -11,7 +11,7 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="AFROTOOAss2",
+            name="AFROTOOAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
