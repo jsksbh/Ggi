059a828000d218e4b10467922116961596ddb401
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-07 15:46:59 +0200

    Update sora.py

--- AFROTOMusic/plugins/play/sora.py
+++ AFROTOMusic/plugins/play/sora.py
@@ -25,8 +25,24 @@ txt = [
 "تم اختيار لك هذه الصوره https://telegra.ph/file/2d15c66003a79cee2db63.jpg",
 "تم اختيار لك هذه الصوره https://telegra.ph/file/49a56d946a59549f0c005.jpg",
 "تم اختيار لك هذه الصوره https://telegra.ph/file/42aa387ab422669b9fd26.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/e348106d5d91c8ba0f5a2.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/2edf7ddcb30f565bb8783.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/226de07b9cd0858ca45a6.jpg", 
+"تم اختيار لك هذه الصوره https://telegra.ph/file/26048e69dc14f8308278a.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/22ab9ef383b082c931ed9.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/a84299faed47ec848e814.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/a51ba9e89034ae5eb58a9.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/c931864e5787b503acbcb.jpg",
+"تم اختيار لك هذه الصوره https://telegra.ph/file/c9311fb1095b53f49e707.jpg",  
+"تم اختيار لك هذه الصوره https://telegra.ph/file/37b7e54cc65e35bb2152a.jpg",  
+"تم اختيار لك هذه الصوره https://telegra.ph/file/7bc310a52e71bdbd0143c.jpg", 
+  "تم اختيار لك هذه الصوره https://telegra.ph/file/45b557af4d2f388ad8272.jpg",
 
-        ]
+
+
+
+
+]
 
 
         
