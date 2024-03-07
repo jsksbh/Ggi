abc64e5fd389262d5cb5d215af97cf07100b3cb1
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-07 20:20:14 +0200

    Update المالك.py

--- AFROTOMusic/plugins/play/المالك.py
+++ AFROTOMusic/plugins/play/المالك.py
@@ -3,17 +3,21 @@ import os
 import time
 import requests
 import aiohttp
-from strings.filters import command
 from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
 from AFROTOMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from AFROTOMusic import app
 from asyncio import gather
 from pyrogram.errors import FloodWait
 
+
+
+
+
 @app.on_message(command(["المالك", "صاحب الخرابه", "المنشي"]) & filters.group)
-async def vgdg(client: Client, message: Message):
+async def gak_owne(client: Client, message: Message):
       if len(message.command) >= 2:
          return 
       else:
@@ -26,9 +30,8 @@ async def vgdg(client: Client, message: Message):
                  m = await client.get_chat(id)
                  if m.photo:
                        photo = await app.download_media(m.photo.big_file_id)
-                       return await message.reply_photo(photo, caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🎃 ¦𝙸𝙳 :{m.id}\n💌 ¦𝙱𝙸𝙾 :{m.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :{message.chat.id}",reply_markup=key)
+                       return await message.reply_photo(photo, caption=f"**- 𝒈𝒓𝒐𝒖𝒑 𝒐𝒘𝒏𝒆𝒓 𝒊𝒏𝒇𝒐𝒓𝒎𝒂𝒕𝒊𝒐𝒏 : \n\n⌁︙ 𝒏𝒂𝒎𝒆 : {m.first_name}\n\n⌁︙ 𝒖𝒔𝒆𝒓 : @{m.username}\n\n⌁︙ 𝒃𝒊𝒐 :\n{m.bio}",reply_markup=key)
                  else:
                     return await message.reply("• " + member.user.mention)
                     
                     
-   
