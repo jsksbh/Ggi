b2a365002c83cb5792ff689fed32bfac8082ba65
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-07 02:10:29 +0200

    Update userbot.py

--- AFROTOMusic/core/userbot.py
+++ AFROTOMusic/core/userbot.py
@@ -11,28 +11,28 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="MatrixAss1",
+            name="AFROTOOAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="MatrixAss2",
+            name="AFROTOOAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="MatrixAss3",
+            name="AFROTOOAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="MatrixAss4",
+            name="AFROTOOAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
