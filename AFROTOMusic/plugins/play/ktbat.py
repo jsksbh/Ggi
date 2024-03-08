c629326827ec20839e0eaa5a00e1f4e094627162
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-08 02:58:36 +0200

    Update ktbat.py

--- AFROTOMusic/plugins/play/ktbat.py
+++ AFROTOMusic/plugins/play/ktbat.py
@@ -79,7 +79,7 @@ txt = [
         
 
 
-@app.on_message(filters.command(["كتبات","حكمه"], ""))
+@app.on_message(filters.command(["كتابات","حكمه","كتابات"], ""))
 
 async def ktbat(client: Client, message: Message):
 
