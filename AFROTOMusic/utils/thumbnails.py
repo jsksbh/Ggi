ec0237005fd1d3304eefadf2b74604a969568e7e
AHMEDMOHAMEDSAYED1 <133281935+AHMEDMOHAMEDSAYED1@users.noreply.github.com>
2024-03-09 23:30:56 +0200

    Update thumbnails.py

--- AFROTOMusic/utils/thumbnails.py
+++ AFROTOMusic/utils/thumbnails.py
@@ -25,157 +25,98 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-def truncate(text):
-    list = text.split(" ")
-    text1 = ""
-    text2 = ""
-    for i in list:
-        if len(text1) + len(i) < 30:
-            text1 += " " + i
-        elif len(text2) + len(i) < 30:
-            text2 += " " + i
+async def gen_bot(client, username, photo):
+        if os.path.isfile(f"{username}.png"):
+           return f"{username}.png"
+        users = len(await get_served_users(client))
+        chats = len(await get_served_chats(client))
+        url = f"https://www.youtube.com/watch?v=gKA2XFkJZhI"
+        results = VideosSearch(url, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
 
-    text1 = text1.strip()
-    text2 = text2.strip()
-    return [text1, text2]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(f"thumb{username}.png", mode="wb")
+                    await f.write(await resp.read())
+                    await f.close()
 
+        youtube = Image.open(f"{photo}")
+        Mostafa = Image.open(f"{photo}")
+        image1 = changeImageSize(1280, 720, youtube)
+        image2 = image1.convert("RGBA")
+        background = image2.filter(filter=ImageFilter.BoxBlur(5))
+        enhancer = ImageEnhance.Brightness(background)
+        background = enhancer.enhance(0.6)
+        Xcenter = Mostafa.width / 2
+        Ycenter = Mostafa.height / 2
+        x1 = Xcenter - 250
+        y1 = Ycenter - 250
+        x2 = Xcenter + 250
+        y2 = Ycenter + 250
+        logo = Mostafa.crop((x1, y1, x2, y2))
+        logo.thumbnail((520, 520), Image.ANTIALIAS)
+        logo = ImageOps.expand(logo, border=15, fill="white")
+        background.paste(logo, (50, 100))
+        draw = ImageDraw.Draw(background)
+        font = ImageFont.truetype("font2.ttf", 40)
+        font2 = ImageFont.truetype("font2.ttf", 70)
+        arial = ImageFont.truetype("font2.ttf", 30)
+        name_font = ImageFont.truetype("font.ttf", 30)
+        draw.text(
+            (600, 150),
+            "Music Player BoT",
+            fill="white",
+            stroke_width=2,
+            stroke_fill="white",
+            font=font2,
+        )
+        draw.text(
+            (600, 340),
+            f"Dev : ALASUOTEY",
+            fill="white",
+            stroke_width=1,
+            stroke_fill="white",
+            font=font,
+        )
+        draw.text(
+            (600, 280),
+            f"Playing Music & Video",
+            fill="white",
+            stroke_width=1,
+            stroke_fill="white",
+            font=font,
+        )
 
-async def get_thumb(videoid):
-    try:
-        if os.path.isfile(f"cache/{videoid}.jpg"):
-            return f"cache/{videoid}.jpg"
-
-        url = f"https://www.youtube.com/watch?v={videoid}"
-        if 1 == 1:
-            results = VideosSearch(url, limit=1)
-            for result in (await results.next())["result"]:
-                try:
-                    title = result["title"]
-                    title = re.sub("\W+", " ", title)
-                    title = title.title()
-                except:
-                    title = "Unsupported Title"
-                try:
-                    duration = result["duration"]
-                except:
-                    duration = "Unknown Mins"
-                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-                try:
-                    views = result["viewCount"]["short"]
-                except:
-                    views = "Unknown Views"
-                try:
-                    channel = result["channel"]["name"]
-                except:
-                    channel = "Unknown Channel"
-
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    f"http://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
-                ) as resp:
-                    if resp.status == 200:
-                        f = await aiofiles.open(f"cache/thumb{videoid}.jpg", mode="wb")
-                        await f.write(await resp.read())
-                        await f.close()
-
-            youtube = Image.open(f"cache/thumb{videoid}.jpg")
-            image1 = changeImageSize(1280, 720, youtube)
-            image2 = image1.convert("RGBA")
-            background = image2.filter(filter=ImageFilter.BoxBlur(30))
-            enhancer = ImageEnhance.Brightness(background)
-            background = enhancer.enhance(0.6)
-            image2 = background
-
-            circle = Image.open("AFROTOMusic/assets/AFYONA.png")
-
-            # changing circle color
-            im = circle
-            im = im.convert("RGBA")
-            color = make_col()
-
-            data = np.array(im)
-            black, lead, blue, alpha = data.T
-
-            white_areas = (black == 255) & (blue == 255) & (lead == 255)
-            data[..., :-1][white_areas.T] = color
-
-            im2 = Image.fromarray(data)
-            circle = im2
-            # done
-
-            image3 = image1.crop((280, 0, 1000, 720))
-            lum_img = Image.new("L", [720, 720], 0)
-            draw = ImageDraw.Draw(lum_img)
-            draw.pieslice([(0, 0), (720, 720)], 0, 360, fill=255, outline="white")
-            img_arr = np.array(image3)
-            lum_img_arr = np.array(lum_img)
-            final_img_arr = np.dstack((img_arr, lum_img_arr))
-            image3 = Image.fromarray(final_img_arr)
-            image3 = image3.resize((600, 600))
-
-            image2.paste(image3, (50, 70), mask=image3)
-            image2.paste(circle, (0, 0), mask=circle)
-
-            # fonts
-            font1 = ImageFont.truetype("AFROTOMusic/assets/font2.ttf", 30)
-            font2 = ImageFont.truetype("AFROTOMusic/assets/font2.ttf", 70)
-            font3 = ImageFont.truetype("AFROTOMusic/assets/font2.ttf", 40)
-            font4 = ImageFont.truetype("AFROTOMusic/assets/font2.ttf", 35)
-
-            image4 = ImageDraw.Draw(image2)
-            image4.text(
-                (10, 10), "AFROTOO MUSIC ", fill="white", font=font1, align="left"
-            )
-            image4.text(
-                (670, 150),
-                "AFROTOO MUSIC",
-                fill="white",
-                font=font2,
-                stroke_width=4,
-                stroke_fill="white",
-                align="left",
-            )
-
-            # title
-            title1 = truncate(title)
-            image4.text(
-                (670, 300),
-                text=title1[0],
-                fill="white",
-                stroke_width=1,
-                stroke_fill="white",
-                font=font3,
-                align="left",
-            )
-            image4.text(
-                (670, 350),
-                text=title1[1],
-                fill="white",
-                stroke_width=1,
-                stroke_fill="white",
-                font=font3,
-                align="left",
-            )
-
-            # description
-            views = f"Views : {views}"
-            duration = f"Duration : {duration} Mins"
-            channel = f"Channel : @UI_VM"
-
-            image4.text((670, 450), text=views, fill="white", font=font4, align="left")
-            image4.text(
-                (670, 500), text=duration, fill="white", font=font4, align="left"
-            )
-            image4.text(
-                (670, 550), text=channel, fill="white", font=font4, align="left"
-            )
-
-            image2 = ImageOps.expand(image2, border=20, fill=make_col())
-            image2 = image2.convert("RGB")
-            image2.save(f"cache/{videoid}.jpg")
-            file = f"cache/{videoid}.jpg"
-            return file
-    except Exception as e:
-        print(e)
-        return YOUTUBE_IMG_URL
-
+        draw.text(
+            (600, 400),
+            f"user : {users}",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (600, 450),
+            f"chats : {chats}",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (600, 500),
+            f"Version : 0.1.5",
+            (255, 255, 255),
+            font=arial,
+        )
+        draw.text(
+            (600, 550),
+            f"BoT : t.me\{username}",
+            (255, 255, 255),
+            font=arial,
+        )
+        try:
+            os.remove(f"thumb{username}.png")
+        except:
+            pass
+        background.save(f"{username}.png")
+        return f"{username}.png"
+        
