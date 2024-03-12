119173f97d56884b3bb52576e4113a0650d67e23
jsksbh <162906565+jsksbh@users.noreply.github.com>
2024-03-12 23:26:21 +0200

    Update *السورس.py

--- AFROTOMusic/plugins/play/*السورس.py
+++ AFROTOMusic/plugins/play/*السورس.py
@@ -14,24 +14,24 @@ from random import  choice, randint
 
                 
 @app.on_message(
-    command(["سورس","‹ السورس ›","عفرتو","السورس", "سورس عفرتو"])
+    command(["سورس","‹ السورس ›","فينوم","السورس", "سورس فينوم "])
 )
 async def huhh(client: Client, message: Message):
     await message.reply_video(
-        video=f"https://telegra.ph/file/0913f5246d0532e170e21.mp4",
+        video=f"https://f.top4top.io/m_2993e37id0.mp4",
         caption=f"""
 ⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼""",
         reply_markup=InlineKeyboardMarkup(
         [
             [
                     InlineKeyboardButton(
-                        " 𝙲𝙷𝙰𝙽𝙽𝙴𝙻  ", url=f"https://t.me/UI_VM"),
+                        " 𝙲𝙷𝙰𝙽𝙽𝙴𝙻  ", url=f"https://t.me/K_o_c_3"),
                    InlineKeyboardButton(
                     
-                    " 𝙶𝚁𝙾𝚄𝙿 ", url=f"https://t.me/YR_HX"), 
+                    " 𝙶𝚁𝙾𝚄𝙿 ", url=f"https://t.me/q1_b1"), 
                 ],[    
                     InlineKeyboardButton(
-                        "𝙰𝙵𝚁𝙾𝚃𝙾𝙾 ", url=f"https://t.me/VVYVVJ"),
+                        "𝚅𝙴𝙽𝙾𝙼♪", url=f"https://t.me/K_o_c_1"),
                 ],[
                     InlineKeyboardButton(
                         "‹ اضف البوت لمجموعتك ›", url=f"https://t.me/A_Rn_obot?startgroup=true"),
